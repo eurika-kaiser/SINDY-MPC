e5e31eb2a6cceb997cd09897a46ad257e7dbb497
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-18 09:32:03 -0700

    Cleaned and commented

--- EX_HIV_THERAPY/DMDc.m
+++ EX_HIV_THERAPY/DMDc.m
@@ -1,11 +1,16 @@
 function [sysmodel_DMDc,U,Up] = DMDc(X1,X2,U,dt)
-numVar = size(X1,1); numOutputs = numVar; numInputs = size(U,1);
-Gamma = U;
-Omega = [X1; Gamma];
+numOutputs = size(X1,1); numInputs = size(U,1);
+Omega = [X1; U];
 [U,S,V] = svd(Omega,'econ'); 
 G = X2*V*S^(-1)*U';
+
+% Needed if reduced-order projection is seeked
 [Up,Sp,Vp] = svd(X2,'econ'); 
-Ar = G(:,1:numVar);
-Br = G(:,numVar+1:end);
-Cr = eye(numVar); 
+
+% Construct system matrices
+Ar = G(:,1:numOutputs);
+Br = G(:,numOutputs+1:end);
+Cr = eye(numOutputs); 
+
+% Discrete-time state- space model
 sysmodel_DMDc = ss(Ar,Br,Cr,zeros(numOutputs,numInputs),dt);
