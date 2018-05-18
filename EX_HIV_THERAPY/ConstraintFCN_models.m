aced60dd278dea73e464f17a91d925f8e34e819f
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-18 09:31:45 -0700

    Cleaned and commented

--- EX_HIV_THERAPY/ConstraintFCN_models.m
+++ EX_HIV_THERAPY/ConstraintFCN_models.m
@@ -5,17 +5,24 @@ function [c, ceq] = ConstraintFCN_models(u,uold,x,N,LBo,UBo,LBdu,UBdu,p,select_m
 %   u:      optimization variable, from time k to time k+N-1 
 %   x:      current state at time k
 %   Ts:     controller sample time
-%   N:      prediction horizon
+%   N:      prediction horizon length
+%   uold:   latest applied control input
+%   LBo:    Lower bound of output x
+%   UBo:    Upper bound of output x
+%   LBdu:   Lower bound for input difference uk - uk-1
+%   UBdu:   Upper bound for input difference uk - uk-1
+%   p:      Parameters for model
+%   select_model: Selects model future-state prediction
 %
 % Output:
 %   c:      inequality constraints applied across prediction horizon
-%   ceq:    equality constraints (empty)
+%   ceq:    equality constraints  (empty)
 %
+
 Nvar = length(x);
 %% Nonlinear MPC design parameters
-% range of angle of attack
-zMin = LBo;
-zMax = UBo;
+% Ensure that all cell populations are positive
+zMin = LBo; 
 
 %% Integrate system
 if strcmp(select_model,'DelayDMDc')
@@ -23,9 +30,6 @@ if strcmp(select_model,'DelayDMDc')
     [xk,~] = lsim(p.sys,Uinput,[0:N-1].*p.dt,p.xdelay);
     xk = xk(:,end-Nvar+1:end);
     xk = xk + repmat(p.xmean',[N 1]); xk = xk';    
-%      [xk,~] = lsim(p.sys,[p.udelay(1:N);u'],[0:N-1].*p.dt,[p.xdelay(:,1); x]-[p.xmean; p.xmean]);
-%     xk = xk(:,Nvar+1:2*Nvar);
-%     xk = xk + repmat(p.xmean',[N 1]); xk = xk';
 elseif strcmp(select_model,'DMDc')
     [xk,~] = lsim(p.sys,[u',0],[0:N].*p.dt,x-p.xmean);
     xk = xk(2:end,:) + repmat(p.xmean',[N 1]); xk = xk'; 
@@ -51,8 +55,8 @@ elseif strcmp(select_model,'partialSINDYc')
     end
     xk = xk(:,2:N+1);     
 elseif strcmp(select_model,'NARX')    
-    Hu = [u',0];%-p.umean,0];
-    Hx = zeros(Nvar,length(Hu)); Hx(:,1) = x;%-p.xmean;
+    Hu = [u',0];
+    Hx = zeros(Nvar,length(Hu)); Hx(:,1) = x;
     if p.TRANSFORM_LOG == 1
         Hx = log(Hx);
     end
@@ -62,44 +66,21 @@ elseif strcmp(select_model,'NARX')
     if p.TRANSFORM_LOG == 1
         xk = exp(xk);
     end
-    xk = xk;% + repmat(p.xmean,[1 size(xk,2)]);
 end
 
     
 
 %% Inequality constraints calculation
 c = zeros(N,1);
-% c = zeros(2*N,1);
 % Apply N population size constraints across prediction horizon, from time
 % k+1 to k+N
-uk = u(1);
-duk = u(1)-uold;
+
 for ct=1:N
     % -z + zMin < 0 % lower bound
     c(ct) = -xk(1,ct)+zMin;
-%     c(2*ct-1) = -xk(1,ct)+zMin; %c(ct)
-%     c(2*ct-1) = -duk+LBdu; 
-    % z - zMax < 0 % upper bound
-%     c(2*ct) = xk(1,ct)-zMax;
-%     c(2*ct) = duk-UBdu;
-    % update  input for next step
-    if ct<N
-        uk = u(ct+1);
-        duk = u(ct+1)-u(ct);
-    end
+
 end
 
-% % cy = zeros(2*N,1); % upper AND lower bound
-% cy = zeros(N,1); % upper OR lower bound
-% for ct=1:N
-%     % -z + zMin < 0 % lower bound
-%     cy(ct) = -xk(1,ct)+zMin; %c(ct)
-% %     cy(2*ct-1) = -xk(1,ct)+zMin; %c(ct)
-%     % z - zMax < 0 % upper bound
-% %     cy(2*ct) = xk(1,ct)-zMax;
-% end
-% 
-% c = [c;cy];
 %% No equality constraints
 ceq = [];
 
