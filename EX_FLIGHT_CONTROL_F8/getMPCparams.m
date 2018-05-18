15417985155ec5f9da184032448a3bb55c15b8da
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-18 13:31:12 -0700

    style: remove commented parts, add description

--- EX_FLIGHT_CONTROL_F8/getMPCparams.m
+++ EX_FLIGHT_CONTROL_F8/getMPCparams.m
@@ -7,8 +7,8 @@ R = 0.05;                       % du weights, 0
 Ru = 0.05;                      % u weights, 0.1
 LB = -0.3*ones(Nu,1);           % Lower bound of control input, -0.3
 UB = 0.5*ones(Nu,1);            % Upper bound of control input, 0.5
-LBdu = nan;                     % Lower bound of control input, -0.1
-UBdu = nan;                     % Upper bound of control input, 0.1
+LBdu = nan;                     % Lower bound of control input rate, -0.1
+UBdu = nan;                     % Upper bound of control input rate, 0.1
 LBo = [-0.2,-1,-1];             % Lower bound of output
 UBo = [0.4,1,1];                % Upper bound of output
 
--- EX_HIV_THERAPY/getMPCparams.m
+++ EX_HIV_THERAPY/getMPCparams.m
@@ -1,13 +1,11 @@
-N  = 24;%24;%18;%12;%13;                        % Prediction horizon (number of iterations)
-Nu  = N;                            % Control horizon (number of iterations)
-
-% Q = [-1 0 0 1 5];                    % State weights
-Q = [1 0 1 0 0];
+N  = 24;                            % Prediction horizon (number of iterations)
+Nu  = N;                            % Control horizon (number of iterations)           
+Q = [1 0 1 0 0];                    % State weights
 R = 0;                              % du weights
 Ru = 1;                             % u weights
 LB = 0*ones(Nu,1);                  % Lower bound of control input
-UB = 1*ones(Nu,1);                % Upper bound of control input
-LBdu = nan;                         % Lower bound of control input
-UBdu = nan;                         % Upper bound of control input
+UB = 1*ones(Nu,1);                  % Upper bound of control input
+LBdu = nan;                         % Lower bound of control input rate
+UBdu = nan;                         % Upper bound of control input rate
 LBo = 0;                            % Lower bound of output
 UBo = nan;                          % Upper bound of output
