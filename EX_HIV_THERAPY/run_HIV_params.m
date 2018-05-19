d0f583742ca8dd767c8b97074cc6c01a4c1ae538
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-19 13:05:25 -0700

    docs: remove parameters of other model

--- EX_HIV_THERAPY/run_HIV_params.m
+++ EX_HIV_THERAPY/run_HIV_params.m
@@ -1,6 +1,6 @@
 lambda1= 1;    % [cells/(mm^3 day)] Production source rate of CD4+ T-cells
 d      = 0.1;  % Death rate of CD4+ T-cells
-alpha1  = 1;  % Infection rate 
+alpha1  = 1;   % Infection rate 
 a      = 0.2;
 p1     = 1; 
 p2     = 1;
@@ -10,25 +10,4 @@ b1     = 0.1;
 b2     = 0.01;
 q      = 0.5;
 eta    = 0.9799;
-h      = .1; % FROM OTHER PAPER
-
-% KWON
-%     lambda1= 10;    % [cells/(mm^3 day)] Production source rate of CD4+ T-cells
-%     d      = 0.01;  % Death rate of CD4+ T-cells
-%     k1     = 0.01;  % Infection rate of wild-type virus
-%     k2     = 0.005; % Infection rate of mutant virus
-%     delta  = 0.5;   % Infected cell death rate
-%     mu     = 0.0001; % The probability of mutation, 0.0001
-%     alpha1 = 10;    % Rate at which wild-type virus is produced from infected cells
-%     alpha2 = 10;    % Rate at which mutant virus is produced from infected cells
-%     c      = 3;     % Virus natural death rate
-
-
-%     lambda=10
-%     d = 0.01
-%     a = 0.5
-%     u = 3
-%     beta = 0.01
-%     beta_m = 0.005
-%     eps = 0.0001 = mu
-%     k=km=10
+h      = .1; 
