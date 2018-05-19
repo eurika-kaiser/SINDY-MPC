7060081fdcc2bbab4faba8b8527da463da0a4a70
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-19 13:08:51 -0700

    docs: add comments

--- EX_HIV_THERAPY/trainSINDYc_HIV.m
+++ EX_HIV_THERAPY/trainSINDYc_HIV.m
@@ -1,4 +1,4 @@
-
+%% Compute time derivative
 % Compute  derivative using fourth order central difference
 % use TVRegDiff if more error
 dx = zeros(length(x)-5,NSindy);
@@ -7,41 +7,38 @@ for i=3:length(x)-3
         dx(i-2,k) = (1/(12*dt))*(-x(i+2,SelectVars(k))+8*x(i+1,SelectVars(k))-8*x(i-1,SelectVars(k))+x(i-2,SelectVars(k)));
     end
 end
-% concatenate
+
+% Concatenate states, input, and time derivatives
 xaug = [x(3:end-3,SelectVars) u(3:end-3,:)];
 dx(:,NSindy+1) = 0*dx(:,1);
-
-
 n = size(dx,2);
 
-% Sparse regression
+%% Sparse regression
+% Construct library
 clear Theta Xi
 Theta = poolData(xaug,n,polyorder,usesine);
-Theta_norm = zeros(size(Theta,2),1); %zeros(size(Theta,2),1);ones
+
+% Normalize library columns
+Theta_norm = zeros(size(Theta,2),1); 
 for i = 1:size(Theta,2)
     Theta_norm(i) = norm(Theta(:,i));
     Theta(:,i) = Theta(:,i)./Theta_norm(i);
 end
 m = size(Theta,2);
 
-if exist('lambda_vec') == 1
+% Estimate parameters
+if exist('lambda_vec') == 1 % choose different penalization for each variable
     Xi = sparsifyDynamicsIndependent(Theta,dx,lambda_vec,n-1);
-else
+else % same penalization for all variables
     Xi = sparsifyDynamics(Theta,dx,lambda,n-1);
 end
 
-% count_lambda = 0;
-% while all(Xi(:)==0)==1 && count_lambda < 5
-%     lambda = lambda/10
-%     Xi = sparsifyDynamics(Theta,dx,lambda,n);
-%     count_lambda = count_lambda + 1;
-% end
-
-str_vars = {'x1','x2','x3','x4','x5','u'};
-str_vars = str_vars([SelectVars,end]);
-
+% Rescale identified coefficients to account for normalization
 for i = 1:size(Theta,2)
     Xi(i,:) = Xi(i,:)./Theta_norm(i);
 end
 
+% Print coefficients
+str_vars = {'x1','x2','x3','x4','x5','u'};
+str_vars = str_vars([SelectVars,end]);
 yout = poolDataLIST(str_vars,Xi,n,polyorder,usesine);
