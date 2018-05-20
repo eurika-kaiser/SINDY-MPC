e079cd2e72555c5ccdc7978cc844c9d3b43ac890
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-20 10:14:46 -0700

    docs: clean up

--- EX_LORENZ/getMPCparams.m
+++ EX_LORENZ/getMPCparams.m
@@ -1,7 +1,6 @@
 Q = [1 1 1];                      % State weights
-R = 0.001;%0.5;                   % du weights
-Ru = 0.001;%0.5;                  % u weights
-% B = [1; 0; 0];
+R = 0.001;                        % du weights
+Ru = 0.001;                       % u weights
 C = eye(Nvar);
 D = 0;
 LB = -50*ones(N,1);        % Lower bound of control input
