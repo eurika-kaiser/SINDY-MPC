1c07989c2c594e3757bd02449921329a88e8f967
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-21 08:28:19 -0700

    fix: lower ymin of plot

--- EX_LORENZ/VIZ_SI_Validation.m
+++ EX_LORENZ/VIZ_SI_Validation.m
@@ -8,13 +8,11 @@ subplot(3,1,1), box on, hold on
 plot([tA(1),tA(1)],[-15 260],':','Color',[0.4,0.4,0.4],'LineWidth',1.5)
 text(tA(1)-30,230,'Training', 'FontSize',12)
 text(5+tA(1),230,'Validation', 'FontSize',12)
-% text(5+tB(1),210,'turned on', 'FontSize',12)
 plot([t;tA],[x(:,1);xA(:,1)],'Color',[.4 .4 .4],'LineWidth',1.5);
 plot(tA,xA(:,1),'k','LineWidth',1.5);
 plot(tB,xB(:,1),'--','Color','r','LineWidth',1.5);
 grid on
 ylim([-25 25])
-% xlabel('Time','FontSize',13)
 ylabel('x_1','FontSize',13)
 set(gca,'FontSize',13)
 subplot(3,1,2), box on, hold on
@@ -53,7 +51,7 @@ clear ph
 figure,box on, hold on, 
 ccolors = get(gca,'colororder');
 plot([tA(1),tA(1)],[min(xB(:)) max([xA(:);xB(:)])],':','Color',[0.4,0.4,0.4],'LineWidth',1.5)
-ylim([min(xB(:)) max([xA(:);xB(:)])])
+ylim([min([xA(:);xB(:)]) max([xA(:);xB(:)])])
 plot(t,x(:,1),'Color',ccolors(1,:),'LineWidth',1);
 plot(t,x(:,2),'Color',ccolors(2,:),'LineWidth',1);
 ph(1) = plot([t;tA],[x(:,1);xA(:,1)],'Color',ccolors(1,:),'LineWidth',1);
@@ -68,7 +66,6 @@ xlabel('Time')
 ylabel('xi')
 t1 = text(tA(1)-5,40,'Training', 'FontSize',12);
 t2 = text(1+tA(1),40,'Validation', 'FontSize',12);
-% legend('Prey (True)','Predator (True)', 'Prey (DMDc)','Predator (DMDc)')
 lh = legend(ph([1,4]),'True',ModelName);
 lh.Position = [lh.Position(1)-0.5,lh.Position(2)-0.15,lh.Position(3:4)];
 set(gca,'LineWidth',1, 'FontSize',14)
@@ -83,9 +80,6 @@ clear ph
 figure,box on, hold on, 
 ccolors = get(gca,'colororder');
 plot([tA(1),tA(1)],[-15 260],':','Color',[0.4,0.4,0.4],'LineWidth',1.5)
-% text(tB(1)-50,max([u uv])-0.1*max([u uv]),'Training', 'FontSize',12,'Color','r')
-% text(10+tB(1),max([u uv])-0.1*max([u uv]),'Validation', 'FontSize',12,'Color','r')
-% text(5+tB(1),210,'turned on', 'FontSize',12)
 plot(t,u,'-k','LineWidth',1);
 plot(tv,uv,'-k','LineWidth',1);
 grid off
