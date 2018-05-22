3ea0a8094a954426435b0ee473b8cfd5598474a7
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-21 22:18:49 -0700

    docs: add comments

--- EX_LORENZ/MPC_Results_SLIDES.m
+++ EX_LORENZ/MPC_Results_SLIDES.m
@@ -1,35 +1,35 @@
+% Plots for paper
+% Visualizations of MPC results
+
 clear all; close all;
 
 figpath = '../../FIGURES/LORENZ_SLIDES/'; mkdir(figpath)
 datapath = '../../DATA2/LORENZ/';
 SystemModel = 'LORENZ';
-InputSignalTypeModel = 'sphs'; 
+InputSignalTypeModel = 'sphs';
 InputSignalType = 'sine2';
 N =10;
 
 % load(fullfile(datapath,['MPC_',SystemModel,'_Results_AllModels_',InputSignalTypeModel,'_',InputSignalType,'_N_',num2str(N),'.mat']),'Results')
 load(fullfile(datapath,['MPC_',SystemModel,'_Results_AllModels_',InputSignalTypeModel,'_',InputSignalType,'_All','_N_',num2str(N),'.mat']))
 
-%% SHow results
-
+%% Show results
 for iM = 1:3
-xplot = Results(iM).x';
-uplot = Results(iM).u';
-figure;
-color_line3(xplot(:,1),xplot(:,2),xplot(:,3),uplot,'LineWidth',2);
-colormap(parula)
-view(30,15)
-axis([-15 5 -25 5 5 45])
-% axis tight
-axis off
-set(gca,'LineWidth',1, 'FontSize',14)
-set(gcf,'Position',[100 100 300 200])
-set(gcf,'PaperPositionMode','auto')
-print('-depsc2', '-loose', '-cmyk', [figpath,'EX_',SystemModel,'_SI_',ModelCollection{iM},'_',InputSignalType,'_PhasePlot_Control.eps']);
+    xplot = Results(iM).x';
+    uplot = Results(iM).u';
+    figure;
+    color_line3(xplot(:,1),xplot(:,2),xplot(:,3),uplot,'LineWidth',2);
+    colormap(parula)
+    view(30,15)
+    axis([-15 5 -25 5 5 45])
+    % axis tight
+    axis off
+    set(gca,'LineWidth',1, 'FontSize',14)
+    set(gcf,'Position',[100 100 300 200])
+    set(gcf,'PaperPositionMode','auto')
+    print('-depsc2', '-loose', '-cmyk', [figpath,'EX_',SystemModel,'_SI_',ModelCollection{iM},'_',InputSignalType,'_PhasePlot_Control.eps']);
 end
 %%
-% xplot  = [x(:,1);xA(2:end,1);xHistory(1,2:end)']
-% xplot  = [x(:,1);xA(2:end,1);xHistory(1,2:end)'];
 iM = 2; ModelName = 'SINDYc';
 
 figure; hold on
@@ -37,7 +37,6 @@ xplot  = [x];
 plot3(xplot(:,1),xplot(:,2),xplot(:,3),'-','Color',0.7*ones(1,3),'LineWidth',2);
 view(30,15)
 axis([-20 20 -25 25 5 45])
-% axis tight
 axis off
 set(gca,'LineWidth',1, 'FontSize',14)
 set(gcf,'Position',[100 100 300 200])
@@ -73,7 +72,6 @@ print('-depsc2', '-loose', '-cmyk', [figpath,'EX_',SystemModel,'_SI_',ModelName,
 
 %%
 figure, box on
-% semilogy(sqrt(sum((xC-xA).^2,2)),'-k','LineWidth',2)
 color_line3(10+(t(2)-t(1)).*[1:length(sqrt(sum((xC-xA).^2,2)))]',sqrt(sum((xC-xA).^2,2)),zeros(size(sqrt(sum((xC-xA).^2,2)))),log(sqrt(sum((xC-xA).^2,2)))-mean(sqrt(sum((xC-xA).^2,2))),'LineWidth',2)
 colormap(jet)
 xlabel('t'), ylabel('E')
