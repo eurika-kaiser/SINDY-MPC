88d8b3b2a821ba9a5f084624a2a2f7a25e21c7a9
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-05-21 22:26:58 -0700

    docs: add comments

--- EX_LORENZ/EX_LORENZ_MPC_Dependency.m
+++ EX_LORENZ/EX_LORENZ_MPC_Dependency.m
@@ -1,39 +1,39 @@
-% LOTKA-VOLTERRA system
-% System identification: DelayDMDc
+% LORENZ system
+% Evaluate ensemble of models (e.g. for varying training data length, noise
+% level, etc.) in MPC
 
 clear all, close all, clc
 addpath('../utils');
 
-WORKING = 1;
+WORKING = 0;
 SystemModel = 'LORENZ';
+
 %% Parameters
 InputSignalType = 'sphs';
 ModelName = 'NARX'; % DMDc, NARX, SINDYc
-TrainAlg = 'trainbr'; %trainlm,trainbr
+TrainAlg = 'trainbr'; %trainlm, trainbr % Choose: trainbr: eta_vec ~= 0, trainlm: eta_vec = 0
 select_model = ModelName;
 dep_trainlength = 1;
 dep_noise = 0;
 
+% Dependency on training length, noise-free training data
 % Ntrain_vec = [5:15,20:5:95,100:100:1000];%,1500:500:3000];
 % eta_vec = 0;
 % Nr = 1;
 
+% Dependency on training length, fixed noise level of data
 Ntrain_vec = [5:15,20:5:95,100:100:1000,1250,1500,2000,3000];
 eta_vec = 0.05;
 Nr = 1;
 
 
+% Some parameters
 N_LENGTHS = length(Ntrain_vec);
 Nmodels = N_LENGTHS;
 
 %% Folders
-if WORKING == 1
-    figpath0 = ['/Users/ekaiser/Documents/Academia/Papers/KaKuBr_SINDYc-MPC/WORKING/FIGURES/EX_',SystemModel,'_Dependencies/'];
-    datapath0 = ['/Users/ekaiser/Documents/Academia/Papers/KaKuBr_SINDYc-MPC/WORKING/DATA/EX_',SystemModel,'_Dependencies/'];
-else
-    figpath0 = ['../../FIGURES/EX_',SystemModel,'_Dependencies/'];
-    datapath0 = ['../../DATA/EX_',SystemModel,'_Dependencies/'];
-end
+figpath0 = ['../../FIGURES/EX_',SystemModel,'_Dependencies/'];
+datapath0 = ['../../DATA/EX_',SystemModel,'_Dependencies/'];
 
 if strcmp(ModelName,'NARX')
     figpath = [figpath0, ModelName, '/',TrainAlg,'/TL/'];
@@ -77,8 +77,7 @@ runMPC
 %% Save results
 save(fullfile(datapath,['EX_',SystemModel,'_MPC_',ModelName,'_',InputSignalType,'_TrainLength','.mat']),'Results')
 
-%%
-
+%% Show performance
 figure,hold on
 for iM = 1:Nmodels
     Jend = cumsum(Results(iM).J);
